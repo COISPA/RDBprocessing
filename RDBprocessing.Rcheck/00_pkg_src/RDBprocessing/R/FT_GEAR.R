9c100e568ed92034fa3b9ec674ca02ff707ba491
COISPA Tecnologia & Ricerca <isabellabitetto@hotmail.it>
2023-04-05 15:38:12 +0200

    cleaned

--- RDBprocessing.Rcheck/00_pkg_src/RDBprocessing/R/FT_GEAR.R
+++
@@ -1,7 +0,0 @@
-#' communication table fishing technique- gear based on Italian FDI data
-#'
-#' @name FT_GEAR
-#' @docType data
-#' @author Isabella Bitetto \email{bitetto@coispa.it}
-#' @keywords fishing technique gear FDI
-"FT_GEAR"
--- RDBprocessing.Rcheck/00_pkg_src/RDBprocessing/R/ce_example.R
+++
@@ -1,7 +0,0 @@
-#' RBD CE example (https://www.ices.dk/data/Documents/RDB/RDB%20Exchange%20Format.pdf)
-#'
-#' @name ce_example
-#' @docType data
-#' @author Isabella Bitetto \email{bitetto@coispa.it}
-#' @keywords RDB, CE, effort_data
-"ce_example"
